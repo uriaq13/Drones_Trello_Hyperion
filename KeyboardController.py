c984f3070e6a11640fd114ed824cedfc60fa5076
uriaq13 <129228510+uriaq13@users.noreply.github.com>
2023-09-22 11:44:05 -0600

    Add files via upload

---
+++ KeyboardController.py
@@ -0,0 +1,35 @@
+from djitellopy import tello
+import KeyPressModule as kp
+from time import sleep
+
+kp.init()
+drone = tello.Tello()
+drone.connect()
+print(drone.get_battery())
+
+def getKeyBoardInput():
+    lr, fb, up, yv = 0, 0, 0, 0
+    speed = 50
+
+    if kp.getKey("LEFT"): lr = -speed
+    elif kp.getKey("RIGHT"): lr = speed
+
+    if kp.getKey("UP"): fb = speed
+    elif kp.getKey("DOWN"): fb = -speed
+
+    if kp.getKey("w"): up = speed
+    elif kp.getKey("s"): up = -speed
+
+    if kp.getKey("a"): yv = speed
+    elif kp.getKey("d"): yv = -speed
+
+    if kp.getKey("q"): drone.land()
+    if kp.getKey("e"): drone.takeoff()
+
+    return [lr, fb, up, yv]
+
+
+while True:
+    vals = getKeyBoardInput()
+    drone.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    sleep(0.05)
